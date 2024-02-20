8e4960bf7a64b5f2b98e0b241e27dacabcdbdd14
Sergey Inshakov <isa86box@gmail.com>
2024-02-20 22:59:09 +0300

    init commit

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -1,14 +1,25 @@
-import { fileURLToPath, URL } from 'url'
-
+// import { fileURLToPath, URL } from 'url'
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: [
+          '@import "./src/assets/scss/app";',
+          '', // end with newline
+        ].join('\n')
+      }
+    }
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, './src')
+      // '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
   server: {
