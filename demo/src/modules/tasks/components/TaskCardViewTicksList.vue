9e6d2e19af362242b14f8d261d4ae3388d828b48
Sergey Inshakov <isa86box@gmail.com>
2024-02-23 17:34:36 +0300

    Создание кнопки добавления новой подзадачи

--- demo/src/modules/tasks/components/TaskCardViewTicksList.vue
+++ demo/src/modules/tasks/components/TaskCardViewTicksList.vue
@@ -1,10 +1,27 @@
 <template>
   <div class="task-card__check-list">
     <h4 class="task-card__title">
-      Чек-лист
+      Чеклист
 
       <!--Кнопка создания новой подзадачи-->
+      <button
+        v-if="!disabled"
+        type="button"
+        class="task-card__plus"
+        @click="$emit('createTick')"
+      />
     </h4>
     <!--Список подзадач-->
   </div>
 </template>
+
+<script setup>
+const props = defineProps({
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const emits = defineEmits(["createTick"]);
+</script>
