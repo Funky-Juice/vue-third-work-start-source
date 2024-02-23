4965cc1eeb7c03dcae0513b74934de3168ef933d
Sergey Inshakov <isa86box@gmail.com>
2024-02-23 17:33:36 +0300

    Создание компонента чек-листа — подзадач

---
+++ demo/src/modules/tasks/components/TaskCardViewTicksList.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="task-card__check-list">
+    <h4 class="task-card__title">
+      Чек-лист
+
+      <!--Кнопка создания новой подзадачи-->
+    </h4>
+    <!--Список подзадач-->
+  </div>
+</template>
