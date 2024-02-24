6f28bd344b53ce6449cc390a2d9f4405e7cee711
Sergey Inshakov <isa86box@gmail.com>
2024-02-24 20:48:55 +0300

    кастомная директива click-outside

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,11 +1,15 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import 'animate.css'
+import "animate.css";
 
 import App from "./App.vue";
 import router from "./router";
 
+import { clickOutside } from "./common/directives";
+
 const app = createApp(App);
+
+app.directive("click-outside", clickOutside);
 
 app.use(router);
 
