8682cbd273147180d8575e10bd1ada4d82c84dcf
Sergey Inshakov <isa86box@gmail.com>
2024-02-25 18:44:04 +0300

    Добавление хранилища в компонент счётчика

---
+++ demo/src/demo4/DemoCounter.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="counter">
+      <counter-button @action="decrement">-</counter-button>
+
+      <counter-value :value="count" />
+
+      <counter-button @action="increment">+</counter-button>
+    </div>
+</template>
+
+
+<script setup>
+import { ref } from 'vue'
+import CounterButton from "demo/src/demo1/CounterButton.vue";
+import CounterValue from "demo/src/demo1/CounterValue.vue";
+
+const count = ref(0)
+
+/* Метод уменьшения значения */
+const decrement = () => {
+  count.value = count.value - 1
+}
+
+/* Метод увеличения значения */
+const increment = () => {
+  count.value = count.value + 1
+}
+</script>
+
+<style lang="scss" scoped>
+.counter {
+  display: flex;
+  align-items: center;
+}
+</style>
