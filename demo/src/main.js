ca928f9c4f6920c0d78130a81865adf97b4e2828
Sergey Inshakov <isa86box@gmail.com>
2024-03-09 00:22:20 +0300

    Восстановление аутентификации после перезагрузки страницы

--- demo/src/main.js
+++ demo/src/main.js
@@ -5,6 +5,8 @@ import "animate.css";
 import App from "./App.vue";
 import router from "./router";
 
+import { getToken, removeToken } from "@/services/token-manager";
+import { useAuthStore } from "@/stores";
 import { clickOutside } from "./common/directives";
 
 const app = createApp(App);
@@ -16,3 +18,16 @@ app.use(router);
 app.use(createPinia());
 
 app.mount("#app");
+
+// Проверяем, если пользователь уже вошёл в систему
+const token = getToken();
+if (token) {
+  try {
+    const authStore = useAuthStore();
+    await authStore.getMe();
+    await router.push("/");
+  } catch (e) {
+    removeToken();
+    console.log(e);
+  }
+}
