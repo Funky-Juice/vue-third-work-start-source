544d8e5d5162ec63d9dd038612eae6f4224c7e74
Sergey Inshakov <isa86box@gmail.com>
2024-03-09 00:12:21 +0300

    Работа над формой входа: стили

--- demo/src/views/AppLogin.vue
+++ demo/src/views/AppLogin.vue
@@ -116,3 +116,116 @@ async function login() {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/app.scss";
+
+.sign-form {
+  display: block;
+
+  width: 500px;
+
+  background-color: $white-900;
+  box-shadow: 0 4px 8px $shadow-500;
+
+  @include pf_center-all;
+
+  &__shape {
+    padding-right: 64px;
+    padding-bottom: 64px;
+    padding-left: 64px;
+  }
+
+  &__header {
+    display: flex;
+    justify-content: space-between;
+
+    padding: 25px;
+
+    background-color: $blue-600;
+  }
+
+  &__title {
+    margin: 30px 0;
+
+    text-align: center;
+
+    color: $blue-gray-600;
+
+    @include m-s24-h21;
+  }
+
+  &__input {
+    display: block;
+
+    margin-bottom: 16px;
+  }
+
+  &__checkbox {
+    margin-top: 20px;
+  }
+
+  &__wrap {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    margin-top: 30px;
+  }
+
+  &__link {
+    color: $blue-600;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  &__close {
+    position: relative;
+
+    width: 20px;
+    height: 20px;
+
+    cursor: pointer;
+
+    border: none;
+    background-color: transparent;
+
+    &::after,
+    &::before {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+
+      width: 100%;
+      height: 1px;
+
+      content: "";
+      transition: background-color $animationSpeed;
+
+      background-color: $white-900;
+    }
+
+    &::after {
+      transform: translate(-50%, -50%) rotate(45deg);
+    }
+
+    &::before {
+      transform: translate(-50%, -50%) rotate(-45deg);
+    }
+
+    &:hover {
+      &::before,
+      &::after {
+        opacity: 0.6;
+      }
+    }
+  }
+}
+
+.server-error-message {
+  margin-top: 20px;
+  color: $red-700;
+}
+</style>
