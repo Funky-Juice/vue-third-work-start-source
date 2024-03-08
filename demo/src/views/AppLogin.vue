4ea98681bc554027c2e9391d529165e010b4f99d
Sergey Inshakov <isa86box@gmail.com>
2024-03-09 00:08:30 +0300

    Работа над формой входа: шапка формы

--- demo/src/views/AppLogin.vue
+++ demo/src/views/AppLogin.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="sign-form sign-in">
     <!--    Шапка формы входа-->
+    <div class="sign-form__header">
+      <router-link to="/" class="logo">
+        <img
+          src="@/assets/img/logo.svg"
+          alt="VueWork logo"
+          width="147"
+          height="23"
+        />
+      </router-link>
 
+      <button
+        class="sign-form__close"
+        type="button"
+        @click="$router.push('/')"
+      />
+    </div>
+
+    <h3 class="sign-form__title">Войти</h3>
     <!--    Форма входа-->
     <form class="sign-form__shape">
       <!--      Поле электронной почты-->
