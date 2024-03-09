cf9f22ad68240bd5d8b978c23c25030c739cfef5
Sergey Inshakov <isa86box@gmail.com>
2024-03-09 18:16:33 +0300

    Изменение компонента TaskCardCreator

--- demo/src/modules/tasks/components/TaskCardCreator.vue
+++ demo/src/modules/tasks/components/TaskCardCreator.vue
@@ -156,7 +156,7 @@
 import { ref, onMounted, watch } from "vue";
 import { useRouter } from "vue-router";
 import { cloneDeep } from "lodash";
-import { useTasksStore } from "@/stores/tasks";
+import { useTasksStore, useTicksStore } from "@/stores";
 
 import { createUUIDv4, createNewDate } from "@/common/helpers";
 import { STATUSES } from "@/common/constants";
@@ -177,7 +177,9 @@ const props = defineProps({
   },
 });
 
+// Определяем хранилище задач
 const tasksStore = useTasksStore();
+const ticksStore = useTicksStore();
 
 // Функция для создания новых задач
 const createNewTask = () => ({
@@ -282,24 +284,41 @@ function removeTick({ uuid, id }) {
   }
   if (id) {
     task.value.ticks = task.value.ticks.filter((tick) => tick.id !== id);
+    ticksStore.deleteTick(id);
   }
 }
 
-function submit() {
+async function submitTicks(taskId, ticks) {
+  const promises = ticks.map((tick) => {
+    if (!tick.text) {
+      return;
+    }
+    delete tick.uuid;
+    tick.taskId = taskId;
+    return tick.id ? ticksStore.updateTick(tick) : ticksStore.addTick(tick);
+  });
+  await Promise.all(promises);
+}
+
+async function submit() {
   // Валидируем задачу
   if (!validateFields(task.value, validations.value)) {
     isFormValid.value = false;
     return;
   }
+  let taskId = task.value.id;
   if (props.taskToEdit) {
     // Редактируемая задача
-    tasksStore.editTask(task.value);
+    await tasksStore.editTask(task.value);
   } else {
     // Новая задача
-    tasksStore.addTask(task.value);
+    const newTask = await tasksStore.addTask(task.value);
+    taskId = newTask.id;
   }
+  // Создать или обновить подзадачи
+  await submitTicks(taskId, task.value.ticks);
   // Переход на главную страницу
-  router.push("/");
+  await router.push("/");
 }
 
 function setTags(tags) {
